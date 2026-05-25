58fd35c9faf1640ce131c2825e68fd02e0b85af5
Nathan Gregory Bornstein <nategbornstein@gmail.com>
2026-05-25 17:50:21 -0500

    Add basic list manipulation

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,34 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  import {ref} from 'vue'
+
+  const restaurantName = ref<string>('')
+  const restaurants = ref<string[]>([])
+
+  function addRestaurant() {
+    const newRestaurants = [...restaurants.value, restaurantName.value]
+    restaurants.value = newRestaurants
+    restaurantName.value = ''
+  }
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit
-    <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to
-    read the documentation
-  </p>
+  <main>
+    <form @submit.prevent="addRestaurant">
+      <label for="restaurant-name">Restaurant Name</label>
+      <input class="input" id="restaurant-name" type="text" v-model="restaurantName"/>
+      <button class="btn" type="submit">Add Restaurant</button>
+    </form>
+    <ul>
+      <li v-for="restaurant in restaurants">{{ restaurant }}</li>
+    </ul>
+  </main>
 </template>
 
-<style scoped></style>
+<style scoped>
+  .input {
+    margin-left: 1rem;
+  }
+  .btn {
+    margin-left: 0.5rem;
+  }
+</style>
