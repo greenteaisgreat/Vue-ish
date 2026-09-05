e78fbb351033640ed3b9ba916ae923681bb49c03
Nathan Gregory Bornstein <nategbornstein@gmail.com>
2026-09-05 10:50:28 -0500

    Add base app logic

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import TaskForm from "./components/TaskForm.vue";
+
+const title = ref("Tasks App");
+const taskArr = ref([]);
+let id = 0;
+
+function addTask(newTask: string) {
+  console.log({ newTask });
+}
+</script>
+
+<template>
+  <main>
+    <h1>{{ title }}</h1>
+    <TaskForm @add-task="addTask" />
+  </main>
+</template>
+
+<style>
+main {
+  max-width: 800px;
+  margin: 1rem auto;
+}
+.button-container {
+  display: flex;
+  justify-content: end;
+}
+</style>
