7746f349a7e667155ac8b9a280d5d2183b5b4d75
Nathan Gregory Bornstein <nategbornstein@gmail.com>
2026-09-05 10:51:02 -0500

    Add <TaskForm /> component

---
+++ src/components/TaskForm.vue
@@ -0,0 +1,27 @@
+<script lang="ts" setup>
+import { ref } from "vue";
+
+const newTask = ref("");
+const emit = defineEmits<{
+  addTask: [newTask: string];
+}>();
+
+function formSubmitted() {
+  console.log(newTask.value);
+
+  emit("addTask", newTask.value);
+  newTask.value = "";
+}
+</script>
+
+<template>
+  <form @submit.prevent="formSubmitted">
+    <label for="newTask">
+      New Task
+      <input name="newTask" v-model="newTask" />
+    </label>
+    <div class="button-container">
+      <button>Add</button>
+    </div>
+  </form>
+</template>
