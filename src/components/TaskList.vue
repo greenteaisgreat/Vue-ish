ce616547769ebbf08f48ecec58d6dd6113c61a4f
Nathan Gregory Bornstein <nategbornstein@gmail.com>
2026-09-14 09:45:06 -0500

    Add checkbox functionality to each task

--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -4,11 +4,32 @@ import type { Task } from "@/types";
 const props = defineProps<{
   tasks: Task[];
 }>();
+const emit = defineEmits<{
+  toggleDone: [id: string];
+}>();
 </script>
 
 <template>
-  <!-- in picoCSS, articles are automatically stylized cards -->
-  <article v-for="task in props.tasks" :key="task.id">
-    {{ task.title }}
-  </article>
+  <div class="task-list">
+    <!-- in picoCSS, articles are automatically stylized cards -->
+    <article v-for="task in props.tasks" :key="task.id">
+      <label>
+        <!-- v-model considered an anti-pattern, since it's modifying state 
+        defined elsewhere, in this case, the parent -->
+        <!-- <input v-model="task.done" type="checkbox" /> -->
+        <input
+          type="checkbox"
+          @input="emit('toggleDone', task.id)"
+          :checked="task.done"
+        />
+        {{ task.title }}
+      </label>
+    </article>
+  </div>
 </template>
+
+<style>
+.task-list {
+  margin-top: 1rem;
+}
+</style>
