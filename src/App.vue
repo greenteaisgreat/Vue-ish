365c5597b0e941fa7b80369ff087cc81b0e771b7
Nathan Gregory Bornstein <nategbornstein@gmail.com>
2026-09-16 09:52:43 -0500

    Add removeTask functionality to App

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,8 @@ import type { Task } from "./types.ts";
 
 const title = ref("Tasks App");
 const tasks = ref<Task[]>([]);
-
+// similar to useMemo in react; it only runs if one its dependencies change
+// computed is used to alter existing reactive data
 const totalDone = computed(() =>
   tasks.value.reduce((total, task) => (task.done ? total + 1 : total), 0),
 );
@@ -18,6 +19,11 @@ function addTask(newTask: string) {
     done: false,
   });
 }
+
+function removeTask(id: string) {
+  tasks.value = tasks.value.filter((task) => task.id !== id);
+}
+
 function toggleDone(id: string) {
   const task = tasks.value.find((task) => task.id === id);
   if (task) {
@@ -33,7 +39,7 @@ function toggleDone(id: string) {
     <h3 v-if="!tasks.length">Add a Task to Get Started</h3>
     <h3 v-else>{{ totalDone }}/{{ tasks.length }} Tasks Completed</h3>
     <!-- syntactic sugar for :tasks="tasks" -->
-    <TaskList :tasks @toggle-done="toggleDone" />
+    <TaskList :tasks @toggle-done="toggleDone" @remove-task="removeTask" />
   </main>
 </template>
 
