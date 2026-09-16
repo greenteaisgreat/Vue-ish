4f46ebe314eee4f3aafb05247de85eb44466671a
Nathan Gregory Bornstein <nategbornstein@gmail.com>
2026-09-16 09:54:04 -0500

    Add TransitionGroup to deleted tasks

--- src/components/TaskList.vue
+++ src/components/TaskList.vue
@@ -6,16 +6,17 @@ const props = defineProps<{
 }>();
 const emit = defineEmits<{
   toggleDone: [id: string];
+  removeTask: [id: string];
 }>();
 </script>
 
 <template>
-  <div class="task-list">
-    <!-- in picoCSS, articles are automatically stylized cards -->
-    <article v-for="task in props.tasks" :key="task.id">
+  <!-- in picoCSS, articles are automatically stylized cards -->
+  <TransitionGroup name="list" tag="div" class="task-list">
+    <article v-for="task in props.tasks" :key="task.id" class="task">
       <label>
-        <!-- v-model considered an anti-pattern, since it's modifying state 
-        defined elsewhere, in this case, the parent -->
+        <!-- v-model considered an anti-pattern, since it's modifying state
+          defined elsewhere, in this case, the parent -->
         <!-- <input v-model="task.done" type="checkbox" /> -->
         <input
           type="checkbox"
@@ -24,8 +25,12 @@ const emit = defineEmits<{
         />
         <span :class="{ done: task.done }">{{ task.title }}</span>
       </label>
+      <!--class outline is picoCSS-->
+      <button class="outline" @click="emit('removeTask', task.id)">
+        Remove
+      </button>
     </article>
-  </div>
+  </TransitionGroup>
 </template>
 
 <style>
@@ -33,7 +38,33 @@ const emit = defineEmits<{
   margin-top: 1rem;
 }
 
+.task {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
 .done {
   text-decoration: line-through;
+}
+
+/* apply transition to moving elements*/
+.list-enter-active,
+.list-leave-active {
+  transition: all 0.5s ease;
+}
+
+.list-enter-from,
+.list-leave-to {
+  opacity: 0;
+  transform: translateX(300px);
+}
+
+/* helps animated remaining items*/
+.list-leave-active {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  position: absolute;
 }
 </style>
