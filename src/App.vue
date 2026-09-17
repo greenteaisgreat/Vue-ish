00c68d2c03814f70b5d1930a2edd70d9064cd0bc
Nathan Gregory Bornstein <nategbornstein@gmail.com>
2026-09-17 17:02:32 -0500

    Import <FilterButton /> and add reactive filtering logic

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,30 @@
 import { ref, computed } from "vue";
 import TaskForm from "./components/TaskForm.vue";
 import TaskList from "./components/TaskList.vue";
-import type { Task } from "./types.ts";
+import FilterButton from "./components/FilterButton.vue";
+import type { Task, TaskFilter } from "./types.ts";
 
 const title = ref("Tasks App");
 const tasks = ref<Task[]>([]);
-// similar to useMemo in react; it only runs if one its dependencies change
+const filter = ref<TaskFilter>("all");
+
+// similar to useMemo in react; it only runs if one its dependencies change.
 // computed is used to alter existing reactive data
 const totalDone = computed(() =>
   tasks.value.reduce((total, task) => (task.done ? total + 1 : total), 0),
 );
+
+const filterTasks = computed(() => {
+  switch (filter.value) {
+    case "all":
+      return tasks.value;
+    case "done":
+      return tasks.value.filter((task) => task.done);
+    case "todo":
+      return tasks.value.filter((task) => !task.done);
+  }
+  return tasks.value;
+});
 
 function addTask(newTask: string) {
   tasks.value.push({
@@ -30,6 +45,10 @@ function toggleDone(id: string) {
     task.done = !task.done;
   }
 }
+
+function setFilter(filterType: TaskFilter) {
+  filter.value = filterType;
+}
 </script>
 
 <template>
@@ -38,8 +57,29 @@ function toggleDone(id: string) {
     <TaskForm @add-task="addTask" />
     <h3 v-if="!tasks.length">Add a Task to Get Started</h3>
     <h3 v-else>{{ totalDone }}/{{ tasks.length }} Tasks Completed</h3>
-    <!-- syntactic sugar for :tasks="tasks" -->
-    <TaskList :tasks @toggle-done="toggleDone" @remove-task="removeTask" />
+    <div v-if="tasks.length" class="button-container">
+      <FilterButton
+        :currentFilter="filter"
+        filter="all"
+        @set-filter="setFilter('all')"
+      />
+      <FilterButton
+        :currentFilter="filter"
+        filter="todo"
+        @set-filter="setFilter('todo')"
+      />
+      <FilterButton
+        :currentFilter="filter"
+        filter="done"
+        @set-filter="setFilter('done')"
+      />
+    </div>
+    <!-- syntactic sugar for :tasks="tasks", used to be just :tasks before 'filterTasks' -->
+    <TaskList
+      :tasks="filterTasks"
+      @toggle-done="toggleDone"
+      @remove-task="removeTask"
+    />
   </main>
 </template>
 
@@ -51,5 +91,6 @@ main {
 .button-container {
   display: flex;
   justify-content: end;
+  gap: 1rem;
 }
 </style>
